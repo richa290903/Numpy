1be90ed605d2e1e308b3dfe25929eb7c15deb5ee
richa290903 <155316586+richa290903@users.noreply.github.com>
2026-01-05 11:36:39 +0530

    numpy

--- return_squared_elements.py
+++ return_squared_elements.py
@@ -1,9 +1,10 @@
 # write a function that takes a numpy array and returns a new array with all elemets squared.
 import numpy as np
 def square(arr):
-      return arr*2
+      return np.square(arr)
 
 arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print("Original array :\n",arr)
 result = square(arr)
+
 print("Squared array :\n",result)
